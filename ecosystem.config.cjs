357f51c667a78d3841cd5dc7b509d9949e50c836
AtVeRcA13 <atverca@gmail.com>
2024-06-12 20:11:54 +0500

    add eco system fail

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'app',
+            port: '3000',
+            exec_mode: 'cluster',
+            instances: 'max',
+            script: './.output/server/index.mjs'
+        }
+    ]
+}
